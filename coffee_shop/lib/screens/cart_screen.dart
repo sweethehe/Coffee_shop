882ec38972a2ca3af2c69ba3f2718c548d885ee2
Maelle AMADOU <amadoumaelle@gmail.com>
2026-01-18 10:44:23 +0100

    <feat>(Cart page): Finishing implementing the cart feature

--- coffee_shop/lib/screens/cart_screen.dart
+++ coffee_shop/lib/screens/cart_screen.dart
@@ -1,5 +1,10 @@
+import 'package:coffee_shop/common/button.dart';
 import 'package:coffee_shop/common/cart_coffe_container.dart';
+import 'package:coffee_shop/common/custom_snack_bar.dart';
 import 'package:coffee_shop/common/data_base.dart';
+import 'package:coffee_shop/screens/bottom_nav_bar.dart';
+import 'package:coffee_shop/screens/home_screen.dart';
+import 'package:coffee_shop/ui/colors.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatefulWidget {
@@ -19,27 +24,63 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: cartCoffee.isEmpty
           ? Center(child: Text("Your cart is empty (◞ ‸ ◟ㆀ)"))
-          : ListView.builder(
-              itemCount: cartCoffee.length,
-              itemBuilder: (context, index) {
-                return cartCoffeeContainer(
-                  cartCoffee[index],
-                  () {
-                    setState(() {
-                      cartCoffee[index]['quantity']++;
-                    });
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cartCoffee.length,
+                    itemBuilder: (context, index) {
+                      return cartCoffeeContainer(
+                        cartCoffee[index],
+                        () {
+                          setState(() {
+                            cartCoffee[index]['quantity']++;
+                          });
+                        },
+                        () {
+                          setState(() {
+                            if (cartCoffee[index]['quantity'] > 1) {
+                              cartCoffee[index]['quantity']--;
+                            } else {
+                              cartCoffee.removeAt(index);
+                            }
+                          });
+                        },
+                      );
+                    },
+                  ),
+                ),
+                customButton(
+                  context: context,
+                  text: "Checkout",
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor: primaryColor,
+                        duration: Duration(seconds: 2),
+                        behavior: SnackBarBehavior.floating,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        content: Text(
+                          'Your order has been placed (˶ˆᗜˆ˵) - See you soon!',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                    cartCoffee.clear();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BottomNavBarScreen()),
+                    );
                   },
-                  () {
-                    setState(() {
-                      if (cartCoffee[index]['quantity'] > 1) {
-                        cartCoffee[index]['quantity']--;
-                      } else {
-                        cartCoffee.removeAt(index);
-                      }
-                    });
-                  },
-                );
-              },
+                ),
+              ],
             ),
     );
   }
